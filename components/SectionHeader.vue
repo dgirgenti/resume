cba73444f950eae9197fec894060622758fb3e15
Dan Girgenti <dangirgenti@gmail.com>
2020-05-17 19:33:00 -0400

    restyle SectionHeader for a11y AA compliance

--- components/SectionHeader.vue
+++ components/SectionHeader.vue
@@ -8,20 +8,22 @@
 export default {
     name: 'SectionHeader',
 };
-</script> 
+</script>
 
 <style lang="scss">
 .section-header {
-    color: #FFF;
-    font-size: 1.1em;
-    margin: 10px 0 0;
-    padding: 8px 8px 8px 20px;
-    background-color: #76923D;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: normal;
+    margin: 0 -30px 10px;
+    padding: 8px 8px 8px 30px;
+    background-color: #657b37;
     text-transform: lowercase;
     letter-spacing: 1px;
+    transition: background-color 250ms;
 
     section:hover & {
-        background-color: #A83582;
+        background-color: #a83582;
     }
 }
 </style>
