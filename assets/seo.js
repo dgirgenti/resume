44421a1777298d8a84b85239f893b591f7bc2ee4
Dan Girgenti <dangirgenti@gmail.com>
2020-05-17 19:32:32 -0400

    update seo

--- assets/seo.js
+++ assets/seo.js
@@ -1,4 +1,4 @@
 export default {
     title: 'Dan Girgenti',
-    description: 'Web engineer with a focus on interface, performance, and scalable architecture for Web applications. Put yourself in the users\' shoes and make something great.',
+    description: 'Philadelphia-based software engineer with a focus on web applications. Advocate for performance-first architecture at scale, responsive interface design systems, and constant knowledge sharing. I dig 4-space tabs and trailing commas.',
 };
