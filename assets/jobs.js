fd03ddbdac286662a2b843bb0181ce2346e33c8c
Dan Girgenti <dangirgenti@gmail.com>
2020-05-17 21:29:04 -0400

    more job updates

--- assets/jobs.js
+++ assets/jobs.js
@@ -16,30 +16,33 @@ export default [
             },
         ],
         description: `
-Urban Outfitters is a mutlinational clothing & lifestyle retailer. The Web team is responsible for the front-end ecommerce platforms of **[Urban Outfitters](https://urbanoutfitters.com)**, **[Anthropologie](https://anthropologie.com)**, and **[Free People](https://freepeople.com/)**.
+Urban Outfitters is a multinational clothing & lifestyle retailer. The Web team is responsible for the front-end ecommerce platforms of **[Urban Outfitters](https://urbanoutfitters.com)**, **[Anthropologie](https://anthropologie.com)**, and **[Free People](https://freepeople.com/)**.
 
-I am currently on the Web Architecture team as a Senior Software Engineer, where I have since been working on a front-end replatform to an isomorphic progressive web application using Vue. The performance-first app architecture heavily utilizes async route- and component-level chunking, dynamic Vuex module registration, lazyloading, and lazy hydration. The project has involved strict performance budgets driving leaner component, server, and data architecture.
+I am currently a Senior Software Engineer, where I have been serving in an architecture role on a front-end replatform to an isomorphic Vue progressive web application. The performance-first app architecture heavily relies on async route- and component-level chunking, dynamic Vuex module registration, lazy loading, and lazy hydration. The project has involved strict performance budgets driving lean component, server, and data architecture.
 
-* Perform constant optimization of app performance
-  * Deprioritazion of background and below-the-fold functionality
-  * Lowering bytes over the wire by decreasing bundle sizes and limiting extraneous data
-  * Analyze and respond to real-time user monitoring with performance-focused changes
-* Advise in architecture matters across the Web teams
-* Onboard Web engineers newer to the Vue platform 1-on-1 and present information during weekly knowledge-sharing sessions
-* Interface with designers & UX to drive design system changes while ensuring performance and WCAG 2.1 AA compliance are not affected
+* Advise in architecture and styling matters across the Web teams
+* Serve as codeowner on all core files for peer review and guidance
+* Ensure constant optimization of app performance
+  * Deprioritization of background and below-the-fold functionality
+  * Decreasing bytes over the wire by decreasing bundle sizes and limiting extraneous data
+  * Compare performance over time using automated Lighthouse scripts
+  * Implement A/B or multivariate tests for partial rollouts, feature comparison, and conversion testing
+* Present information during weekly knowledge-sharing sessions
+* Served as styling architect from January to April 2019, overseeing the design system, SCSS mixin, and UI component libraries, and style guides on Storybook
 
 Prior to October 2018, I worked on the Python 3 / Tornado platform with a custom JS component library and Redux, supporting mainly the product detail and account pages.`,
         technologies: [
             'JavaScript (es5 to es2020)',
+            'Python 3',
+            'Tornado',
             'Server-side Vue',
             'Vuex',
             'vue-router',
             'vue-i18n',
             'SCSS',
-            'Python 3',
-            'Tornado',
             'Redux',
             'Webpack',
+            'Docker',
             'nginx',
             'express',
             'axios',
@@ -48,11 +51,15 @@ Prior to October 2018, I worked on the Python 3 / Tornado platform with a custom
             'Nunjucks',
             'Lighthouse',
             'PWMetrics',
+            'Storybook',
             'WCAG 2.1 AA',
+            'Contentful',
+            'Sentry',
             'Jenkins',
             'nosetest',
             'jest',
             'jasmine',
+            'Jira',
         ],
     },
     {
@@ -68,9 +75,9 @@ Prior to October 2018, I worked on the Python 3 / Tornado platform with a custom
         description: `
 Zoomi is an artificial intelligence platform for learning which provides first-of-its-kind prescriptive analytics to instructors. I was a full-stack developer responsible for various projects from the UI to the cloud.
 
-While at Zoomi I implemented and maintained several production features. Most notably, I architected and built Zoomi's asset management and content ingestion systems, which were responsible for processing and adapting an increasing list of supported media types and learning standards into the production client application.
+While at Zoomi I implemented and maintained several production features. Most notably, I architected and built Zoomi's asset management and content ingestion systems, which processed and adapted an increasing list of supported media types and learning standards into the production client application.
 
-* Built new features on both the Django API and Backbone client application
+* Built features on both the Django API and Backbone client application
 * Maintained asset management processes and systems
 * Wrote scripts to ingest content and media from a variety of client-provided sources
 * Interfaced with production clients and researchers about technical specifications and requirements`,
@@ -78,8 +85,9 @@ While at Zoomi I implemented and maintained several production features. Most no
             'Python',
             'Django',
             'AWS',
+            'Docker',
             'nginx',
-            'JavaScript',
+            'JavaScript (es6)',
             'Backbone.js',
             'Webpack',
             'gulp',
@@ -98,17 +106,16 @@ While at Zoomi I implemented and maintained several production features. Most no
             },
         ],
         description: `
-Club OS is a health club CRM and Web platform. I was one of two front-end engineers, and together we worked alongside the designer and back-end team to develop new features for the platform.
+Club OS is a health club CRM and Web platform. I was one of two front-end engineers, and together we worked alongside the designer and back-end team to develop features for the platform.
 
-My largest project was a responsive UI kit based around Bootstrap. The kit served as the authoritative source of UI component styling for all future internal and external Web apps, as well as the re-skinning of the existing app. It contained various styles, themes, custom JavaScript components such as a datepicker, and a custom SVG icon font.
+My largest project was a responsive UI kit based around Bootstrap. The kit served as the authoritative source of UI component styling for all future internal and external Web apps, and the re-skinning of the existing app. It contained various styles, themes, custom JavaScript components such as a datepicker, and a custom SVG icon font.
 
 * Refactored, cleaned, and maintained front-end codebase
 * Automated commonly-performed tasks
 * Created and modified custom (non-)embedded forms as requested by external stakeholders
 * Participated in team planning and discussions related to front-end architecture and workflow`,
         technologies: [
-            'JavaScript',
-            'es6',
+            'JavaScript (es6)',
             'Node',
             'gulp',
             'HTML',
@@ -132,11 +139,11 @@ My largest project was a responsive UI kit based around Bootstrap. The kit serve
             },
         ],
         description: `
-Ticketleap is a DIY ticketing and event services provider. I joined the team during undergrad, and learned full-stack development on their Python & Django stack. I was involved in every aspect of the software development lifecycle by participating in research, design, architecture, development, testing, and maintenance, UI to cloud. I had partial responsibility for other duties like system administration, SEO, and gathering requirements from or assisting members of the Customer Success, Marketing, Accounting, and QA teams.
+Ticketleap is a DIY ticketing and event services provider. I joined the team during undergrad and learned full-stack development on their Python & Django stack. I was involved in every aspect of the software development lifecycle by participating in research, design, architecture, development, testing, and maintenance, UI to cloud. I had partial responsibility for other duties like system administration, SEO, and gathering requirements from or assisting members of the Customer Success, Marketing, Accounting, and QA teams.
 
 * Prototyped and developed responsive user interfaces with HTML/CSS and jQuery
 * Designing and implementing features on front- and back-end Django stacks in parallel with QA
-* Performed a variety of maintenance, sysadmin, and devops duties`,
+* Performed a variety of maintenance, sysadmin, and devops duties on multiple AWS products (S3, EC2, ELB)`,
         technologies: [
             'Python',
             'Django',
@@ -179,7 +186,7 @@ Ticketleap is a DIY ticketing and event services provider. I joined the team dur
                 end: 'September 2012',
             },
         ],
-        description: 'Razorfish Health is a healthcare-focused design/ad agency. During this six-month internship, I turned Photoshop and Fireworks designs into functional websites with HTML/CSS/jQuery. I served as front-end dev on a C#/ASP.NET/MVC3 project for one of the largest pharmacy benefit management organizations in the US. I also fixed style issues on two Webby Honoree sites and created a bunch of HTML emails for two of the top five pharmaceutical companies in the US.',
+        description: 'Razorfish Health is a healthcare-focused design/ad agency. During this six-month internship, I turned Photoshop and Fireworks designs into functional websites with HTML/CSS/jQuery. I served as front-end dev on a C#/ASP.NET/MVC3 project for one of the largest pharmacy benefit management organizations in the US. Improved styling on two Webby Honoree sites and created a bunch of HTML emails for two of the top five pharmaceutical companies in the US.',
         technologies: [
             'HTML',
             'CSS',
